9e03d50c45080f0bc7f45654219c4ae5350a4813
RasulDzuahunov <rasul.dju@gmail.com>
2024-12-18 21:57:12 +0600

    Add files via upload

---
+++ DZ2_Rasul_43-1F/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import MainPage from './pages/MainPage';
+
+const App = () => {
+  return (
+    <div>
+      <MainPage />
+    </div>
+  );
+};
+
+export default App;
+
